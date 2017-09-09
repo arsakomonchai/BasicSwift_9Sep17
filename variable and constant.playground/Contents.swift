e32d82cf4218b764a7f8d0c7ee777643c0979450
keng-volunteerr <arsa_ko@hotmail.com>
2017-09-09 10:14:33 +0700

    test

---
+++ variable and constant.playground/Contents.swift
@@ -0,0 +1,16 @@
+var//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร ภาษา swift
+//Implicit
+
+var nameString = "keng-Volunteer"
+nameString = "doramon"
+
+//Constant
+
+let SurnameString = "ABCD"
+//SurnameString = "ABCDED" //Error เพราะ let แก้ไขไม่ได้
+
+
