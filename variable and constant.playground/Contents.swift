65637514d53460f195f9524bd34c39339194b72c
keng-volunteerr <arsa_ko@hotmail.com>
2017-09-09 11:53:31 +0700

    Integer

--- variable and constant.playground/Contents.swift
+++ variable and constant.playground/Contents.swift
@@ -1,4 +1,4 @@
-var//: Playground - noun: a place where people can play
+//: Playground - noun: a place where people can play
 
 import UIKit
 
@@ -12,5 +12,18 @@ nameString = "doramon"
 
 let SurnameString = "ABCD"
 //SurnameString = "ABCDED" //Error เพราะ let แก้ไขไม่ได้
+
+
+//Print
+print("name => \(nameString)")
+
+
+//about Interger
+let number1AInt = 5
+let number2AInt: Int = 10  //นี้คือการประกาศแบบ กำหนด Type data ว่าเป็น Int
+
+
+let answr = number1AInt + number2AInt
+print("answer ==> \(answr)")
 
 
